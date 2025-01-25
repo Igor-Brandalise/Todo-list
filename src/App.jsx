18018a407c6bfc5440c85b69cac5f0a1505e0668
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-01-24 23:21:54 -0300

    create TodoForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 
 import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -34,6 +36,7 @@ function App() {
           <Todo todo = {todo}/>
         ))}
       </div>
+      <TodoForm/>
     </div>
   );
 }
