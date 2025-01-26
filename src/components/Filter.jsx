b62651b6252fa443d7d7055ca868e6934d1ad054
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-01-26 01:32:35 -0300

    função filtrar e função ordem alfabética

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,11 @@
-const Filter = () => {
+const Filter = ({filter, setFilter, setSort}) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
       <div className="filter-options">
         <div>
           <p>Status:</p>
-          <select>
+          <select valu={filter} onChange={(e) => setFilter(e.target.value)}>
             <option value="All">Todas</option>
             <option value="Completed">Completas</option>
             <option value="Incomplete">Incompletas</option>
@@ -13,9 +13,9 @@ const Filter = () => {
         </div>
 
         <div>
-            <p>Ordem alfabética:</p>
-            <button>Asc</button>
-            <button>Desc</button>
+          <p>Ordem alfabética:</p>
+          <button onClick={() => setSort("Asc")}>Asc</button>
+          <button onClick={() => setSort("Desc")}>Desc</button>
         </div>
       </div>
     </div>
