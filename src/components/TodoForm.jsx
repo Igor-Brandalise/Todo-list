30bda816dfea6be9a0b84c3da719174188ecb834
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-01-26 00:30:30 -0300

    limpar o form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) {
+      return;
+    }
+
+    setValue("");
+    setCategory("");
+    console.log(value, category);
+  };
+
   return (
     <div className="todo-form">
       <h2>Criar Tarefa</h2>
-      <form>
-        <input type="text" placeholder="Digite o Titulo" />
-        <select>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o Titulo"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="">Selecione uma categoria</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
